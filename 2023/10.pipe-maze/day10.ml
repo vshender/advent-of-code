c3b29d3dc8993cf6ae77590ca626fec0ef8c215f
Vadim Shender <vadim@shender.org>
2023-12-10 20:10:39 +0000

    2023, Day 10: Pipe Maze (refactoring)

--- 2023/10.pipe-maze/day10.ml
+++ 2023/10.pipe-maze/day10.ml
@@ -1,132 +1,110 @@
 (** Day 10: Pipe Maze *)
 
-(** The type of directions. *)
-type direction = North | South | East | West
+module Direction = struct
+  (** The type of directions. *)
+  type t = North | South | East | West
 
-(** The type of pipes. *)
-type pipe =
-  | NorthSouth  (* | *)
-  | EastWest    (* - *)
-  | NorthEast   (* L *)
-  | NorthWest   (* J *)
-  | SouthEast   (* F *)
-  | SouthWest   (* 7 *)
-  | Ground      (* . *)
-  | Start       (* S *)
+  (** [opposite dir] is the direction opposite to [dir]. *)
+  let opposite = function
+    | North -> South
+    | South -> North
+    | East  -> West
+    | West  -> East
 
-(** [parse_pipe c] parses a character representation of a pipe. *)
-let parse_pipe = function
-  | '|' -> NorthSouth
-  | '-' -> EastWest
-  | 'L' -> NorthEast
-  | 'J' -> NorthWest
-  | 'F' -> SouthEast
-  | '7' -> SouthWest
-  | '.' -> Ground
-  | 'S' -> Start
-  | _   -> failwith "parse_pipe"
+  (** [move pos direction] computes a new position by moving one step from
+      [pos] in the specified [direction]. *)
+  let move (x, y) = function
+    | North -> (x - 1, y)
+    | South -> (x + 1, y)
+    | East  -> (x, y + 1)
+    | West  -> (x, y - 1)
+end
 
-(** [parse_maze lines] parses [lines] and gets the maze. *)
-let parse_maze lines =
-  lines
-  |> List.map (fun s -> Array.init (String.length s) (fun i -> parse_pipe s.[i]))
-  |> Array.of_list
+module Pipe = struct
+  include Set.Make (struct type t = Direction.t let compare = compare end)
 
-(** [find_starting_position maze] finds the starting position in [maze]. *)
-let find_starting_position maze =
-  let exception Found of int * int in
-  try
-    for i = 0 to Array.length maze - 1 do
-      for j = 0 to Array.length maze.(0) - 1 do
-        if maze.(i).(j) = Start then
-          raise (Found (i, j))
-      done
-    done;
-    failwith "find_starting_position"
-  with
-  | Found (i, j) -> (i, j)
+  (** [is_valid_move dir dst_pipe] checks if moving in the specified direction
+      [dir] is valid for the given destination pipe [dst_pipe]. *)
+  let is_valid_move dir dst_pipe = mem (Direction.opposite dir) dst_pipe
 
-(** [move pos direction] computes a new position by moving one step from [pos]
-    in the specified [direction]. *)
-let move (x, y) = function
-  | North -> (x - 1, y)
-  | South -> (x + 1, y)
-  | East  -> (x, y + 1)
-  | West  -> (x, y - 1)
+  (** [next_direction pipe prev_dir] determines the next direction to move in,
+      based on the current pipe and the previous movement direction. *)
+  let next_direction pipe prev_dir =
+    remove (Direction.opposite prev_dir) pipe |> choose
 
-(** [is_valid_move dir dst_pipe] checks if moving in the specified direction
-    [dir] is valid for the given destination pipe [dst_pipe]. *)
-let is_valid_move dir dst_pipe =
-  let valid_moves = [
-    North, [NorthSouth; SouthEast; SouthWest];
-    South, [NorthSouth; NorthEast; NorthWest];
-    East,  [EastWest;   NorthWest; SouthWest];
-    West,  [EastWest;   NorthEast; SouthEast];
-  ] in
-  valid_moves |> List.assoc dir |> List.mem dst_pipe
+  (** [parse c] parses a character representation of a pipe. *)
+  let parse = function
+    | '|' -> of_list [North; South]
+    | '-' -> of_list [East; West]
+    | 'L' -> of_list [North; East]
+    | 'J' -> of_list [North; West]
+    | 'F' -> of_list [South; East]
+    | '7' -> of_list [South; West]
+    | '.' -> empty
+    | 'S' -> of_list [North; South; East; West]
+    | _   -> failwith "parse_pipe"
+end
 
-(** [find_first_move maze pos] finds a valid move from the given starting
+(** [find_starting_position lines] parses [lines] and finds the starting
     position. *)
-let find_first_move maze (i, j) =
-  let n = Array.length maze and m = Array.length maze.(0) in
-  List.find
-    (fun dir ->
-       let i, j = move (i, j) dir in
-       0 <= i && i < n && 0 <= j && j < m && is_valid_move dir maze.(i).(j))
-    [North; South; East; West]
+let find_starting_position lines =
+  lines
+  |> List.mapi (fun i line -> String.index_opt line 'S' |> Option.map (fun j -> i, j))
+  |> List.filter_map Fun.id
+  |> List.hd
 
-(** [next_direction pipe prev_dir] determines the next direction to move in,
-    based on the current pipe and the previous movement direction. *)
-let next_direction pipe prev_dir =
-  match pipe, prev_dir with
-  | NorthSouth, North | NorthEast, West  | NorthWest, East  -> North
-  | NorthSouth, South | SouthEast, West  | SouthWest, East  -> South
-  | EastWest,   East  | NorthEast, South | SouthEast, North -> East
-  | EastWest,   West  | NorthWest, South | SouthWest, North -> West
-  | _                                                       -> failwith "next_direction"
+(** [parse_maze lines starting_position] parses [lines] and gets the maze. *)
+let parse_maze lines =
+  let (si, sj) = find_starting_position lines
+  and maze =
+    lines
+    |> List.map (fun s -> Array.init (String.length s) (fun i -> Pipe.parse s.[i]))
+    |> Array.of_list
+  in
+  (* Leave only valid directions at the starting position. *)
+  maze.(si).(sj) <- Pipe.filter
+      (fun dir ->
+         let (i, j) = Direction.move (si, sj) dir in
+         Pipe.is_valid_move dir maze.(i).(j))
+      maze.(si).(sj);
+  (si, sj), maze
 
-(** [find_loop maze] finds a loop path in [maze] starting from a designated
+(** [find_loop maze] finds a loop path in [maze] starting from the given
     starting position. *)
-let find_loop maze =
+let find_loop maze (si, sj) =
   let rec find_loop_aux acc (i, j) dir =
-    match maze.(i).(j) with
-    | Start -> List.rev acc
-    | pipe  ->
-      let dir = next_direction pipe dir in
-      let i, j = move (i, j) dir in
+    if i = si && j = sj then
+      List.rev acc
+    else
+      let dir = Pipe.next_direction maze.(i).(j) dir in
+      let i, j = Direction.move (i, j) dir in
       find_loop_aux ((i, j) :: acc) (i, j) dir
   in
-  let (i, j) = find_starting_position maze in
-  let dir = find_first_move maze (i, j) in
-  find_loop_aux [(i, j)] (move (i, j) dir) dir
+  let dir = Pipe.choose maze.(si).(sj) in
+  let i, j = Direction.move (si, sj) dir in
+  find_loop_aux [(i, j)] (i, j) dir
 
 (** [find_area_inside_loop maze] calculates the area inside a loop in
     [maze]. *)
-let find_area_inside_loop maze =
+let find_area_inside_loop maze (si, sj) =
   let module Positions = Set.Make (struct type t = int * int let compare = compare end) in
-  let loop = Positions.(List.fold_right add (find_loop maze) empty) in
+  let loop = Positions.(List.fold_right add (find_loop maze (si, sj)) empty) in
   let m = Array.length maze.(0) in
-  let rec count_inside_row count is_inside i j =
+  let rec count_in_row count inside i j =
     if j = m then
       count
     else
     if Positions.mem (i, j) loop then
-      count_inside_row
-        count
-        (is_inside <> List.mem maze.(i).(j) [NorthSouth; NorthEast; NorthWest])
-        i (j + 1)
+      count_in_row count (inside <> Pipe.mem North maze.(i).(j)) i (j + 1)
     else
-      count_inside_row
-        (count + if is_inside then 1 else 0)
-        is_inside
-        i (j + 1)
+      count_in_row (count + if inside then 1 else 0) inside i (j + 1)
   in
   maze
-  |> Array.mapi (fun i row -> count_inside_row 0 false i 0)
+  |> Array.mapi (fun i row -> count_in_row 0 false i 0)
   |> Array.fold_left (+) 0
 
 let () =
   let lines = open_in "input" |> In_channel.input_lines in
-  let maze = parse_maze lines in
-  Printf.printf "Part One: %d\n" (List.length (find_loop maze) / 2);
-  Printf.printf "Part Two: %d\n" (find_area_inside_loop maze)
+  let start, maze = parse_maze lines in
+  Printf.printf "Part One: %d\n" (List.length (find_loop maze start) / 2);
+  Printf.printf "Part Two: %d\n" (find_area_inside_loop maze start)
