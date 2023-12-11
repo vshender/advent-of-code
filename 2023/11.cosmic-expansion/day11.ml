c627f0a5e3eaf24bfc2a062054e3682e1eac04ea
Vadim Shender <vadim@shender.org>
2023-12-11 10:50:28 +0000

    2023, Day 11: Cosmic Expansion, part two

--- 2023/11.cosmic-expansion/day11.ml
+++ 2023/11.cosmic-expansion/day11.ml
@@ -1,5 +1,7 @@
 (** Day 11: Cosmic Expansion *)
 
+module IntSet = Set.Make(Int)
+
 (** [parse_image lines] parses [lines] and gets the image of universe. *)
 let parse_image lines =
   lines
@@ -10,34 +12,16 @@ let parse_image lines =
 let rec range a b =
   if a < b then a :: range (a + 1) b else []
 
-(** [count p lst] counts the number of elements in [lst] that satisfy the
-    predicate [p]. *)
-let count p =
-  let rec count_iter count = function
-    | []     -> count
-    | h :: t -> count_iter (count + if p h then 1 else 0) t
-  in count_iter 0
+(** [rows_to_expand image] finds all the rows of [image] to expand. *)
+let rows_to_expand image =
+  let check_row i = Array.for_all ((=) '.') image.(i) in
+  range 0 (Array.length image) |> List.filter check_row |> IntSet.of_list
 
-(** [expand_image image] expands the given image of universe. *)
-let expand_image image =
+(** [columns_to_expand image] finds all the columns of [image] to expand. *)
+let columns_to_expand image =
   let n = Array.length image and m = Array.length image.(0) in
-
-  let check_row i = Array.for_all ((=) '.') image.(i)
-  and check_column j = range 0 n |> List.for_all (fun i -> image.(i).(j) = '.') in
-  let rows_to_expand = range 0 n |> List.filter check_row
-  and columns_to_expand = range 0 m |> List.filter check_column in
-
-  let image' = Array.make_matrix
-      (n + List.length rows_to_expand) (m + List.length columns_to_expand) '.'
-  in
-  for i = 0 to n - 1 do
-    let i' = i + count (fun row -> row < i) rows_to_expand in
-    for j = 0 to m - 1 do
-      let j' = j + count (fun column -> column < j) columns_to_expand in
-      image'.(i').(j') <- image.(i).(j)
-    done
-  done;
-  image'
+  let check_column j = range 0 n |> List.for_all (fun i -> image.(i).(j) = '.') in
+  range 0 m |> List.filter check_column |> IntSet.of_list
 
 (** [get_galaxies image] returns a list of galaxies from [image]. *)
 let get_galaxies image =
@@ -50,9 +34,20 @@ let get_galaxies image =
   done;
   !galaxies
 
-(** [find_shortest_paths (n, m) (i, j) galaxies] finds the shortest paths from
-    the point [(i, j)] to all the [galaxies]. *)
-let find_shortest_paths (n, m) (i, j) galaxies =
+(** [distance (rows_to_expand, columns_to_expand) expansion_factor (i, j) (i', j')]
+    calculates the distance between two adjacent positions [(i, j)] and
+    [(i', j')]. *)
+let distance (rows_to_expand, columns_to_expand) expansion_factor (i, j) (i', j') =
+  if i <> i' then
+    if IntSet.mem i rows_to_expand then expansion_factor else 1
+  else
+    if IntSet.mem j columns_to_expand then expansion_factor else 1
+
+(** [find_shortest_paths (n, m) (rows_to_expand, columns_to_expand) expansion_factor (i, j) galaxies]
+    finds the shortest paths from the point [(i, j)] to all the [galaxies]. *)
+let find_shortest_paths
+    (n, m) (rows_to_expand, columns_to_expand) expansion_factor (i, j) galaxies =
+  let dist = distance (rows_to_expand, columns_to_expand) expansion_factor in
   let dists = Array.make_matrix n m (-1) in
   let q = Queue.create () in
   dists.(i).(j) <- 0;
@@ -63,25 +58,28 @@ let find_shortest_paths (n, m) (i, j) galaxies =
       (fun (di, dj) ->
          let i' = i + di and j' = j + dj in
          if 0 <= i' && i' < n && 0 <= j' && j' < m && dists.(i').(j') = -1 then begin
-           dists.(i').(j') <- dists.(i).(j) + 1;
+           dists.(i').(j') <- dists.(i).(j) + dist (i, j) (i', j');
            Queue.push (i', j') q
          end)
   done;
   galaxies |> List.map (fun (i, j) -> dists.(i).(j))
 
-(** [part1 image] finds the sum of the lengths of the shortest paths between
-    all pairs of galaxies in [image]. *)
-let part1 image =
+(** [solve image expansion_factor] finds the sum of the lengths of the shortest
+    paths between all pairs of galaxies in [image]. *)
+let solve image expansion_factor =
   let n = Array.length image and m = Array.length image.(0) in
+  let re = rows_to_expand image and ce = columns_to_expand image in
   let galaxies = get_galaxies image in
-  let sum = galaxies
-            |> List.map (fun (i, j) -> find_shortest_paths (n, m) (i, j) galaxies)
-            |> List.flatten
-            |> List.fold_left (+) 0
+  let sum =
+    galaxies
+    |> List.map (fun (i, j) ->
+        find_shortest_paths (n, m) (re, ce) expansion_factor (i, j) galaxies)
+    |> List.flatten
+    |> List.fold_left (+) 0
   in sum / 2
 
 let () =
   let lines = open_in "input" |> In_channel.input_lines in
   let image = parse_image lines in
-  let image = expand_image image in
-  Printf.printf "Part One: %d\n" (part1 image)
+  Printf.printf "Part One: %d\n" (solve image 2);
+  Printf.printf "Part Two: %d\n" (solve image 1_000_000)
