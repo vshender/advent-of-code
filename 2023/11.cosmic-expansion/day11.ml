2fbbb65dac931dce43382e017ea3ce074af32811
Vadim Shender <vadim@shender.org>
2023-12-12 10:39:55 +0000

    2023, Day 11: Cosmic Expansion (even simpler solution)

--- 2023/11.cosmic-expansion/day11.ml
+++ 2023/11.cosmic-expansion/day11.ml
@@ -1,61 +1,65 @@
 (** Day 11: Cosmic Expansion *)
 
-(** [parse_image lines] parses [lines] and gets the image of universe. *)
+open List
+
+(** [parse_image lines] parses [lines] and returns the galaxies. *)
 let parse_image lines =
   lines
-  |> List.map (fun line -> Array.init (String.length line) (fun i -> line.[i]))
-  |> Array.of_list
+  |> mapi (fun i line ->
+      line
+      |> String.to_seqi
+      |> Seq.filter (fun (_, c) -> c = '#')
+      |> Seq.map (fun (j, _) -> (i, j))
+      |> of_seq)
+  |> flatten
+
+(** [get_size galaxies] returns the size of the universe. *)
+let get_size galaxies =
+  let (n, m) =
+    fold_left (fun (mi, mj) (i, j) -> (max mi i, max mj j)) (0, 0) galaxies
+  in (n + 1, m + 1)
 
 (** [range a b] generates a list of integers from [a] to [b - 1]. *)
 let rec range a b =
   if a < b then a :: range (a + 1) b else []
 
-(** [rows_to_expand image] finds all the rows of [image] to expand. *)
-let rows_to_expand image =
-  let check_row i = Array.for_all ((=) '.') image.(i) in
-  range 0 (Array.length image) |> List.filter check_row
+(** [rows_to_expand galaxies n] finds all the rows of the universe to
+    expand. *)
+let rows_to_expand galaxies n =
+  range 0 n |> filter (fun x -> galaxies |> for_all (fun (i, _) -> x <> i))
 
-(** [columns_to_expand image] finds all the columns of [image] to expand. *)
-let columns_to_expand image =
-  let n = Array.length image and m = Array.length image.(0) in
-  let check_column j = range 0 n |> List.for_all (fun i -> image.(i).(j) = '.') in
-  range 0 m |> List.filter check_column
+(** [columns_to_expand galaxies m] finds all the columns of the universe to
+    expand. *)
+let columns_to_expand galaxies m =
+  range 0 m |> filter (fun y -> galaxies |> for_all (fun (_, j) -> y <> j))
 
-(** [get_galaxies image] returns a list of galaxies from [image]. *)
-let get_galaxies image =
-  let galaxies = ref [] in
-  for i = 0 to Array.length image - 1 do
-    for j = 0 to Array.length image.(0) - 1 do
-      if image.(i).(j) = '#' then
-        galaxies := (i, j) :: !galaxies
-    done
-  done;
-  !galaxies
+(** [expand galaxies expansion_factor] expands the universe using
+    [expansion_factor]. *)
+let expand galaxies expansion_factor =
+  let n, m = get_size galaxies in
+  let re = rows_to_expand galaxies n and ce = columns_to_expand galaxies m in
+  galaxies
+  |> map (fun (i, j) ->
+      (i + (re |> filter (fun r -> r < i) |> length) * (expansion_factor - 1), j))
+  |> map (fun (i, j) ->
+      (i, j + (ce |> filter (fun c -> c < j) |> length) * (expansion_factor - 1)))
 
-(** [distance (rows_to_expand, columns_to_expand) expansion_factor (i, j) (i', j')]
-    calculates the shortest distance between two positions [(i, j)] and
-    [(i', j')]. *)
-let distance (rows_to_expand, columns_to_expand) expansion_factor (i, j) (i', j') =
-  let i, i' = min i i', max i i' and j, j' = min j j', max j j' in
-  let nr = rows_to_expand |> List.filter (fun x -> i <= x && x <= i') |> List.length
-  and nc = columns_to_expand |> List.filter (fun y -> j <= y && y <= j') |> List.length
-  in (i' - i + nr * (expansion_factor - 1)) + (j' - j + nc * (expansion_factor - 1))
+(** [distance a b] calculates Manhattan distance between [a] and [b]. *)
+let distance (i, j) (i', j') = abs (i - i') + abs (j - j')
 
-(** [solve image expansion_factor] finds the sum of the lengths of the shortest
-    paths between all pairs of galaxies in [image]. *)
-let solve image expansion_factor =
-  let re = rows_to_expand image and ce = columns_to_expand image in
-  let dist = distance (re, ce) expansion_factor in
-  let galaxies = get_galaxies image in
+(** [solve galaxies expansion_factor] finds the sum of the lengths of the
+    shortest paths between all pairs of [galaxies]. *)
+let solve galaxies expansion_factor =
+  let galaxies = expand galaxies expansion_factor in
   let sum =
     galaxies
-    |> List.map (fun from -> List.map (dist from) galaxies)
-    |> List.flatten
-    |> List.fold_left (+) 0
+    |> map (fun from -> map (distance from) galaxies)
+    |> flatten
+    |> fold_left (+) 0
   in sum / 2
 
 let () =
   let lines = open_in "input" |> In_channel.input_lines in
-  let image = parse_image lines in
-  Printf.printf "Part One: %d\n" (solve image 2);
-  Printf.printf "Part Two: %d\n" (solve image 1_000_000)
+  let galaxies = parse_image lines in
+  Printf.printf "Part One: %d\n" (solve galaxies 2);
+  Printf.printf "Part Two: %d\n" (solve galaxies 1_000_000)
