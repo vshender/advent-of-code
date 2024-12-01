abe720b8df88a5dc931499b4845362f195309b59
Vadim Shender <vadzim@shender.org>
2024-12-01 23:25:59 +0000

    2024, Day 1: Historian Hysteria, part two

--- 2024/01.historian-hysteria/day01.ml
+++ 2024/01.historian-hysteria/day01.ml
@@ -16,7 +16,16 @@ let total_distance left right =
     (List.sort Int.compare right)
   |> List.fold_left (+) 0
 
+(** [similarity_score left right] is the similarity score between [left] and
+    [right]. *)
+let similarity_score left right =
+  left
+  |> List.map
+    (fun n -> List.filter (Int.equal n) right |> List.length |> (( * ) n))
+  |> List.fold_left (+) 0
+
 let () =
   In_channel.with_open_text "input" @@ fun ic ->
   let left, right = In_channel.input_lines ic |> parse_location_ids in
-  Printf.printf "Part One: %d\n" @@ total_distance left right
+  Printf.printf "Part One: %d\n" @@ total_distance left right;
+  Printf.printf "Part Two: %d\n" @@ similarity_score left right
