7614fa863b080322dbb18c96e146b540584bc837
Vadim Shender <vadim@shender.org>
2024-12-14 22:34:15 +0000

    2024, Day 12: Garden Groups, part two

--- 2024/12.garden-groups/day12.ml
+++ 2024/12.garden-groups/day12.ml
@@ -7,63 +7,116 @@ let parse_map lines =
   |> List.map (fun line -> Array.init (String.length line) (String.get line))
   |> Array.of_list
 
-(** [dirs] defines the four possible movement directions. *)
+(** The four possible movement directions. *)
 let dirs = [(-1, 0); (0, 1); (1, 0); (0, -1)]
 
-(** A set of int pairs. *)
-module IntPairSet = Set.Make (struct type t = (int * int) let compare = compare end)
+(** The four directions to identify adjacent corners of a plot. *)
+let corners = [(-1, -1); (-1, 1); (1, 1); (1, -1)]
 
-(** [inside_map n m i j] checks whether the position [(i, j)] is within the
-    bounds of the map. *)
-let inside_map n m i j =
+(** [adjacent_to_corner corner] computes adjacent positions for a given
+    corner. *)
+let adjacent_to_corner (i, j) = [(0, j); (i, 0)]
+
+(** A set of plots. *)
+module PlotSet = Set.Make (struct type t = (int * int) let compare = compare end)
+
+(** [inside_map n m (i, j)] checks whether the position [(i, j)] is within the
+    bounds of a map of size [n] x [m]. *)
+let inside_map n m (i, j) =
   0 <= i && i < n && 0 <= j && j < m
 
-(** [region_perimeter_and_area map i j] computes the region, perimeter, and
-    area for a connected group of identical plots starting from the position
-    [(i, j)]. *)
-let region_perimeter_and_area map i j =
+(** [area map region] is the area of [region] as the number of plots it
+    contains. *)
+let area _ = PlotSet.cardinal
+
+(** [perimeter map region] computes the perimeter of [region]. *)
+let perimeter map region =
+  let n = Array.length map and m = Array.length map.(0) in
+  PlotSet.fold
+    (fun (i, j) p ->
+       p +
+       List.fold_left
+         (fun p (di, dj) ->
+            let i' = i + di and j' = j + dj in
+            p + if not (inside_map n m (i', j')) || map.(i').(j') <> map.(i).(j) then 1 else 0)
+         0 dirs)
+    region 0
+
+(** [is_external_corner map (i, j) (di, dj)] checks if the corner [(i, j)] in
+    direction [(di, dj)] is an external corner of a region. *)
+let is_external_corner map (i, j) (di, dj) =
+  let n = Array.length map and m = Array.length map.(0) in
+  List.for_all
+    (fun (di, dj) ->
+       not (inside_map n m (i + di, j + dj)) || map.(i + di).(j + dj) <> map.(i).(j))
+    (adjacent_to_corner (di, dj))
+
+(** [is_internal_corner map (i, j) (di, dj)] checks if the corner [(i, j)] in
+    direction [(di, dj)] is an internal corner of a region. *)
+let is_internal_corner map (i, j) (di, dj) =
+  let n = Array.length map and m = Array.length map.(0) in
+  (inside_map n m (i + di, j + dj) && map.(i + di).(j + dj) <> map.(i).(j)) &&
+  List.for_all
+    (fun (di, dj) ->
+       inside_map n m (i + di, j + dj) && map.(i + di).(j + dj) = map.(i).(j))
+    (adjacent_to_corner (di, dj))
+
+(** [sides_number map region] computes the total number of sides of [region].
+    The function actually calculates the number of corners of [region], which
+    is equal to the number of sides. *)
+let sides_number map region =
+  PlotSet.fold
+    (fun (i, j) n ->
+       n +
+       List.fold_left
+         (fun n (di, dj) ->
+            n
+            + (if is_external_corner map (i, j) (di, dj) then 1 else 0)
+            + (if is_internal_corner map (i, j) (di, dj) then 1 else 0))
+         0 corners)
+    region 0
+
+(** [find_region map (i, j)] identifies and returns the connected region
+    starting from the position [(i, j)]. *)
+let find_region map (i, j) =
   let n = Array.length map and m = Array.length map.(0) in
   let q = Queue.create () in
-  let rec loop region perimeter area =
+  let rec loop region =
     match Queue.take_opt q with
-    | None        -> (region, perimeter, area)
+    | None        -> region
     | Some (i, j) ->
-      let region, plot_perimeter =
-        List.fold_left
-          (fun (region, p) (di, dj) ->
-             let i' = i + di and j' = j + dj in
-             let region =
-               if not (IntPairSet.mem (i', j') region) &&
-                  inside_map n m i' j' && map.(i).(j) = map.(i').(j') then begin
-                 Queue.add (i', j') q;
-                 IntPairSet.add (i', j') region
-               end else
-                 region
-             in
-             (region,
-              p + if inside_map n m i' j' && map.(i).(j) = map.(i').(j') then 0 else 1))
-          (region, 0)
-          dirs
-      in
-      loop region (perimeter + plot_perimeter) (area + 1)
+      loop
+        (List.fold_left
+           (fun region (di, dj) ->
+              let i' = i + di and j' = j + dj in
+              if not (PlotSet.mem (i', j') region) &&
+                 inside_map n m (i', j') && map.(i).(j) = map.(i').(j') then begin
+                Queue.add (i', j') q;
+                PlotSet.add (i', j') region
+              end else
+                region)
+           region
+           dirs)
   in
   Queue.add (i, j) q;
-  loop (IntPairSet.singleton (i, j)) 0 0
+  loop (PlotSet.singleton (i, j))
 
-(** [solve map] computes the total price of all regions in the garden map. *)
-let solve map =
-  let seen = ref IntPairSet.empty and total_price = ref 0 in
+(** [solve perim map] calculates the total price of fencing all regions on the
+    garden map. *)
+let solve perim map =
+  let seen = ref PlotSet.empty and total_price = ref 0 in
   for i = 0 to Array.length map - 1 do
     for j = 0 to Array.length map.(0) - 1 do
-      if not (IntPairSet.mem (i, j) !seen) then
-        let region, perimeter, area = region_perimeter_and_area map i j in
-        seen := IntPairSet.union !seen region;
-        total_price := !total_price + (perimeter * area)
+      if not (PlotSet.mem (i, j) !seen) then
+        let region = find_region map (i, j) in
+        seen := PlotSet.union !seen region;
+        total_price := !total_price + (perim map region * area map region)
     done
   done;
   !total_price
 
-let _ =
+let () =
   In_channel.with_open_text "input" @@ fun ic ->
   let map = In_channel.input_lines ic |> parse_map in
-  Printf.printf "Part One: %d\n" @@ solve map
+  Printf.printf "Part One: %d\n" @@ solve perimeter map;
+  Printf.printf "Part Two: %d\n" @@ solve sides_number map
