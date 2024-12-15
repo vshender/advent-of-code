80fdf483b5dd0c2f25c5d17cd68a7ed97a5e899f
Vadim Shender <vadim@shender.org>
2024-12-15 18:33:05 +0000

    2024, Day 13: Claw Contraption, part two

--- 2024/13.claw-contraption/day13.ml
+++ 2024/13.claw-contraption/day13.ml
@@ -18,28 +18,30 @@ let parse_machines =
     | _ -> assert false
   in loop []
 
-(** [solve_eq machine] finds the number of times buttons A and B need to be
-    pressed to position the claw over the prize for the given
-    [claw_machine]. *)
-let solve_machine { a = xa, ya; b = xb, yb; prize = xp, yp } =
+(** [solve_eq is_valid machine] finds the number of times buttons A and B need
+    to be pressed to position the claw over the prize for the given
+    [claw_machine].  [is_valid] is a function that checks if a number of button
+    presses is valid.
+
+    The function computes result by solving the following system of equations:
+
+    - xa * a + xb * b = xp
+    - ya * a + yb * b = yp
+*)
+let solve_machine is_valid { a = xa, ya; b = xb, yb; prize = xp, yp } =
   let ka = xa * yb - ya * xb and kp = xp * yb - yp * xb in
   if ka = 0 then
-    None
+    if kp = 0 && xp mod xb = 0 then Some (0, xp / xb) else None
   else
     let a = kp / ka and ra = kp mod ka in
-    if ra > 0 || a < 0 || a > 100 then
+    if ra <> 0 || not (is_valid a) then
       None
     else
-      let kb = xb + yb in
-      if kb = 0 then
+      let b = (xp - xa * a) / xb and rb = (xp - xa * a) mod xb in
+      if rb <> 0 || not (is_valid b) then
         None
       else
-        let b = (xp + yp - xa * a - ya * a) / kb
-        and rb = (xp + yp - xa * a - ya * a) mod kb in
-        if rb > 0 || b < 0 || b > 100 then
-          None
-        else
-          Some (a, b)
+        Some (a, b)
 
 (** [tokens_number (a, b)] calculates the number of tokens required to press
     button A [a] times and button B [b] times. *)
@@ -47,14 +49,19 @@ let tokens_number (a, b) = a * 3 + b
 
 (** [solve machines] computes the minimum number of tokens required to
     win prizes from as many claw machines as possible. *)
-let solve machines =
+let solve is_valid machines =
   machines
-  |> List.map solve_machine
+  |> List.map (solve_machine is_valid)
   |> List.filter_map Fun.id
   |> List.map tokens_number
   |> List.fold_left (+) 0
 
-let _ =
+let fix_position { a; b; prize = xp, yp } =
+  { a; b; prize = xp + 10_000_000_000_000, yp + 10_000_000_000_000 }
+
+let () =
   In_channel.with_open_text "input" @@ fun ic ->
   let machines = In_channel.input_lines ic |> parse_machines in
-  Printf.printf "Part One: %d\n" @@ solve machines
+  Printf.printf "Part One: %d\n" @@ solve (fun n -> 0 <= n && n <= 100) machines;
+  let machines' = List.map fix_position machines in
+  Printf.printf "Part Two: %d\n" @@ solve (fun n -> 0 <= n) machines'
